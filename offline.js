2e8a79cf5dd55c98a5f83b3abfd61178f7f62d76
chosensczbo <chosen.scz.bo@gmail.com>
2025-01-14 23:55:21 -0400

    1

---
+++ offline.js
@@ -0,0 +1,5 @@
+UpUp.start({
+    'content-url': '/offline.html',
+    'assets': ['/chartmann/view/img/logo-gb', '/web/view/img/address.png', '/web/view/img/phone.png'],
+    'service-worker-url': '/upup.sw.js'
+});
